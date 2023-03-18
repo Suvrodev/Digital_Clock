cc20b634d0485b5eb857740cd512b1aea871a2a1
Suvrodev Howlader <suvrodev.cse@yahoo.com>
2023-03-18 14:58:06 +0600

    Added second Clock

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 console.log('Digital Clock')
+///First Clock Start
 const Clock=()=>{
     const Hour_1=document.getElementById('hour_');
     const Minutes_1=document.getElementById('min_');
@@ -14,4 +15,63 @@ const Clock=()=>{
 
 }
 
+Clock();
 setInterval(Clock,1000);
+
+//First Clock End
+
+///Second Clock Start
+const UpdateClock=()=>{
+    let Now=new Date();
+    let dayName=Now.getDay();
+    let monthName=Now.getMonth();
+    let dateName=Now.getDate();
+    let yearName=Now.getFullYear();
+    let hourName=Now.getHours();
+    let minName=Now.getMinutes();
+    let secName=Now.getSeconds();
+    let pe='AM';
+    let All_Month=['January','February','March','April','May','June','July','August','September','October','November','December'];
+    let Week=['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+    document.getElementById('dayname').innerHTML=Week[dayName-1];
+    document.getElementById('daynum').innerHTML=dateName;
+    document.getElementById('year').innerHTML=yearName;
+    document.getElementById('_hour').innerHTML=hourName;
+    document.getElementById('_min').innerHTML=minName;
+    document.getElementById('_sec').innerHTML=secName;
+    console.log('Hour: '+hourName+' Datatype: '+typeof hourName)
+   
+    
+    if(hourName>11){
+        document.getElementById('_period').innerHTML='PM';
+    }else{
+        document.getElementById('_period').innerHTML='AM';
+    }
+    if(hourName>12){
+        let newHourName=hourName-12;
+        document.getElementById('_hour').innerHTML=newHourName;
+
+        if(newHourName.toString().length===1){
+            document.getElementById('_hour').innerHTML='0'+newHourName;
+        }
+        if(newHourName===12){
+            document.getElementById('_hour').innerHTML='00';
+        }
+    }
+
+    if(secName.toString().length==1){
+        document.getElementById('_sec').innerHTML='0'+secName;
+    }
+
+    if(minName.toString().length==1){
+        document.getElementById('_min').innerHTML='0'+minName;
+    }
+
+    if(hourName.toString().length==1){
+        document.getElementById('_hour').innerHTML='0'+hourName;
+    }
+}
+
+UpdateClock();
+setInterval(UpdateClock,1000);
+///Second Clock End
